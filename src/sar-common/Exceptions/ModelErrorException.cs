0ef242ec6165768da986014e0ab3f2995ff5207a
Matt Cosand <matt@cosand.net>
2016-11-11 06:58:44 -0800

    model error exception

---
+++ src/sar-common/Exceptions/ModelErrorException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sar
+{
+  public class ModelErrorException : Exception
+  {
+    public ModelErrorException(string property, string error) : this(new Dictionary<string, IEnumerable<string>> { { property, new[] { error } } }, null)
+    {
+    }
+
+    public ModelErrorException(Dictionary<string, IEnumerable<string>> propertyErrors, string generalError) : base()
+    {
+      PropertyErrors = propertyErrors;
+      Error = generalError;
+    }
+
+    public string Error { get; private set; }
+    public Dictionary<string, IEnumerable<string>> PropertyErrors { get; private set; }
+  }
+}
