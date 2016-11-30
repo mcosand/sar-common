8e70148ece71a51d534a910af570839aef763826
Matt Cosand <matt@cosand.net>
2016-11-30 08:49:05 -0800

    Training Courses

---
+++ src/database-api-client/Model/Training/TrainingCourse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sar.Database.Model.Training
+{
+  public class TrainingCourse : NameIdPair
+  {
+    public string FullName { get; set; }
+    public string Category { get; set; }
+    public NameIdPair Unit { get; set; }
+    public int? ValidMonths { get; set; }
+    public NameIdPair[] Prerequisites { get; set; }
+  }
+}
