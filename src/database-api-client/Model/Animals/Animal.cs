143b7c70f38a8a08ded80c949aed83ee4c1ec6b9
Matt Cosand <matt@cosand.net>
2016-12-14 17:03:12 -0800

    animal ownership

---
+++ src/database-api-client/Model/Animals/Animal.cs
@@ -0,0 +1,15 @@
+﻿using Sar.Database.Model.Members;
+
+namespace Sar.Database.Model.Animals
+{
+  public class Animal : NameIdPair
+  {
+    public string Type { get; set; }
+    public MemberSummary Owner { get; set; }
+    public string Status { get; set; }
+
+    public string Comments { get; set; }
+    public string IdSuffix { get; set; }
+    public string Photo { get; set; }
+  }
+}
---
+++ src/database-api-client/Model/Animals/AnimalOwner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Sar.Database.Model.Members;
+
+namespace Sar.Database.Model.Animals
+{
+  public class AnimalOwner : IId
+  {
+    public Guid Id { get; set; }
+    public MemberSummary Member { get; set; }
+    public DateTimeOffset Starting { get; set; }
+    public DateTimeOffset? Ending { get; set; }
+  }
+}
