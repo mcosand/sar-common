ebb4ceff953fefd254c26df71fefbc0359dd2faa
Matt Cosand <matt@cosand.net>
2017-02-02 17:14:17 -0800

    domain model for member address

---
+++ src/database-api-client/Model/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Sar.Database.Model
+{
+  public class Address
+  {
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Zip { get; set; }
+  }
+}
---
+++ src/database-api-client/Model/Members/MemberAddress.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sar.Database.Model.Members
+{
+  public class MemberAddress : Address
+  {
+    public Guid Id { get; set; }
+    public MemberAddressType Type { get; set; }
+  }
+
+  public enum MemberAddressType
+  {
+    /// <summary>Other Address.</summary>
+    Other = 0,
+    /// <summary>Work Address.</summary>
+    Work = 10,
+    /// <summary>The address at which the person receives their mail, other than their residence.</summary>
+    Mailing = 20,
+    /// <summary>The location of the person's residence (where they might get picked up for a mission).</summary>
+    Residence = 30
+  }
+}
