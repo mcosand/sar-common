c6895e14b422090d0a8da3b10f3d0c0cf29eca9a
Matt Cosand <matt@cosand.net>
2017-02-15 22:37:27 -0800

    move event attendance

--- src/database-api-client/Model/Events/EventAttendance.cs
+++ src/database-api-client/Model/Events/EventAttendance.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sar.Database.Model
 {
-  public class EventAttendance
+  public class EventAttendance : IId
   {
+    public Guid Id { get; set; }
     public EventSummary Event { get; set; }
     public int Miles { get; set; }
     public double Hours { get; set; }
---
+++ src/database-api-client/Model/Events/EventYearSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace Sar.Database.Model.Events
+{
+  public class EventYearSummary
+  {
+    public int Year { get; set; }
+    public int Count { get; set; }
+    public int Participants { get; set; }
+    public int Miles { get; set; }
+    public double Hours { get; set; }
+  }
+}
